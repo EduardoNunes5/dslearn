f74fc4b691ba75280067a73fbc9ecc18a6264749
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2022-09-20 18:46:13 -0300

    fixed

--- backend/src/main/java/com/devsuperior/dslearnbds/repositories/SectionRepository.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/SectionRepository.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslearnbds.repositories;
 
+import com.devsuperior.dslearnbds.entities.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SectionRepository extends JpaRepository<SectionRepository, Long> {
+public interface SectionRepository extends JpaRepository<Section, Long> {
 }
